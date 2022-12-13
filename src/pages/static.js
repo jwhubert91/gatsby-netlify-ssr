ce5f912de2ae90dac412ec1804e5ea64cc1a4b78
James Hubert <jwhubert91@gmail.com>
2022-12-13 17:20:06 -0500

    add some static routes

---
+++ src/pages/static.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function StaticPage() {
+  return (
+    <div>
+      This page is very static and should work fine. There's no server side
+      anything happening.
+    </div>
+  )
+}
+
+export default StaticPage
