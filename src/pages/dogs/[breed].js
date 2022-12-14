dd29ad8a5faa931839089ca600cbcbfd486f9b14
Josh Dobson <joshua@joshuarule.com>
2022-12-13 22:46:42 -0800

    updates test

--- src/pages/dogs/[breed].js
+++ src/pages/dogs/[breed].js
@@ -1,16 +1,39 @@
-import React from "react"
-import { useParams } from "@reach/router"
-import DogImage from "../../components/DogImage"
+import React from "react";
+import DogImage from "../../components/DogImage";
 
-function BreedPage() {
-  const params = useParams()
-  console.log(params)
+function BreedPage({ serverData }) {
+  const { breed, imageUrl } = serverData;
   return (
     <div>
-      Breed Page
-      <DogImage />
+      <p>Image of a {breed}.</p>
+      <DogImage imageSrc={imageUrl} />
     </div>
-  )
+  );
 }
 
-export default BreedPage
+export default BreedPage;
+
+export async function getServerData(context) {
+  const { breed } = context.params;
+  try {
+    const res = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(`Response failed`);
+    }
+
+    return {
+      props: {
+        breed,
+        imageUrl: data.message,
+      },
+    };
+  } catch (error) {
+    return {
+      status: 200,
+      headers: {},
+      props: {},
+    };
+  }
+}
